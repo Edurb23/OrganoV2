58f2c5b48d732b60e7135028552a8aef4448e66a
Edurb23 <eddurb64@gmail.com>
2023-09-18 23:43:37 -0300

    Finalizando a aula-4

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,12 +4,16 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './formulario.css'
 
-const Formulario = ({aoCadastrar, times}) => {
+const Formulario = ({aoCadastrar, times, cadastrarTime}) => {
 
     const [nome, setNome] = useState('')
     const [funcao, setFuncao] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
+    const [nomeTime, setnomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
+
+    
 
     const aoSubmeter = (evento) => {
         evento.preventDefault()
@@ -49,6 +53,30 @@ const Formulario = ({aoCadastrar, times}) => {
                     valor={time}
                     aoAlterado={valor => setTime(valor)}/>
                 <Botao texto='Criar card' />
+            </form>
+            <form className="formulario" onSubmit={(evento => {
+                evento.preventDefault()
+                cadastrarTime({
+                    nome: nomeTime,
+                    cor: corTime
+                })
+            })}>
+                <h2>Preencha os dados para criar um novo Time.</h2>
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite o nome do time... '
+                    valor={nomeTime}
+                    aoAlterado={valor => setnomeTime(valor)}/>
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Cor' 
+                    placeholder='Digite a cor '
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}/>
+               
+                
+                <Botao texto='Criar Time' />
             </form>
         </section>
     )
