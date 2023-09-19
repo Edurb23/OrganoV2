f8cad86e813fd7689a80124357f64d755087b1c5
Edurb23 <eddurb64@gmail.com>
2023-09-19 00:27:34 -0300

    finalizando a V2

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
       favorito: false,
       nome: 'Pereira',
       funcao: 'Líbero e Atacaque',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144000952282644500/308691658_451106247032488_4846731854346827528_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144776659950637208/308691658_451106247032488_4846731854346827528_n.png',
       time: times[0].nome
     },
     {
@@ -74,7 +74,7 @@ function App() {
       favorito: false,
       nome: 'Tutz',
       funcao: 'Ataque',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144001374741344327/356948005_963785254868493_5620456784910647675_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144776775461777589/356948005_963785254868493_5620456784910647675_n.png',
       time: times[0].nome
     },
     {
@@ -82,7 +82,7 @@ function App() {
       favorito: false,
       nome: 'Venom',
       funcao: 'Ataque',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144001781462994954/115963818_1906571352811816_7664623793072943811_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144776989413216317/115963818_1906571352811816_7664623793072943811_n.png',
       time: times[0].nome
     },
     {
@@ -90,7 +90,7 @@ function App() {
       favorito: false,
       nome: 'DRE',
       funcao: 'Suporte',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144002856962248734/199816113_324203292415460_4413049102583737756_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144777332121415740/199816113_324203292415460_4413049102583737756_n.png',
       time: times[1].nome
     },
     {
@@ -98,7 +98,7 @@ function App() {
       favorito: false,
       nome: 'Kenjola',
       funcao: 'Suporte',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144003735740219512/309264407_117328681123858_8214617194974107823_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144777407673413662/309264407_117328681123858_8214617194974107823_n.png',
       time: times[1].nome
     },
     {
@@ -106,7 +106,7 @@ function App() {
       favorito: false,
       nome: 'Rafinha',
       funcao: 'Intermediario',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144003977403453510/358784413_807379174341392_5409141725673998462_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144777508034711662/358784413_807379174341392_5409141725673998462_n.png',
       time: times[1].nome
     },
     {
@@ -122,7 +122,7 @@ function App() {
       favorito: false,
       nome: 'Ramsés',
       funcao: 'Entry Fragger',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144004501716615168/image.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144777658241130636/342709001_1205837763428877_5699560682985321964_n.png',
       time: times[1].nome
     },
     {
@@ -138,7 +138,7 @@ function App() {
       favorito: false,
       nome: 'Vitor',
       funcao: 'Sniper',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144006809032609972/352586668_212050495056584_5448251560540989256_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144778557701238916/352586668_212050495056584_5448251560540989256_n.png',
       time: times[2].nome
     },
     {
@@ -146,7 +146,7 @@ function App() {
       favorito: false,
       nome: 'Thiago 30hz.',
       funcao: 'runner',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144006849344057425/252074506_1039610453541161_1519815840931859481_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144778557701238916/352586668_212050495056584_5448251560540989256_n.png',
       time: times[2].nome
     },
     {
@@ -154,7 +154,7 @@ function App() {
       favorito: false,
       nome: 'Eric',
       funcao: 'Suporte',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144006897238814740/312286832_2387945218031558_1071025777643420287_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144778678870474885/312286832_2387945218031558_1071025777643420287_n.png',
       time: times[2].nome
     },
     {
@@ -170,7 +170,7 @@ function App() {
       favorito: false,
       nome: 'Lucas',
       funcao: 'Duelista',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144408155418279957/299732017_2042422376147857_3630607100324684288_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144779724334301305/299732017_2042422376147857_3630607100324684288_n.png',
       time: times[3].nome
     },
     {
@@ -178,7 +178,7 @@ function App() {
       favorito: false,
       nome: 'Rypg',
       funcao: 'Controlador',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144408547564728401/317314942_151751010930259_8331211450778795133_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144780016572436510/317314942_151751010930259_8331211450778795133_n.png',
       time: times[3].nome
     },
     {
@@ -186,7 +186,7 @@ function App() {
       favorito: false,
       nome: 'Omega',
       funcao: 'Flex',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144408916466335824/277860008_480664297137725_6779275824018707741_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144780059014598736/277860008_480664297137725_6779275824018707741_n.png',
       time: times[3].nome
     },
     {
@@ -194,7 +194,7 @@ function App() {
       favorito: false,
       nome: 'JV',
       funcao: 'Iniciador',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144409362945814548/317776196_1564385357419738_679226328360930564_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1144780858646073404/317776196_1564385357419738_679226328360930564_n.png',
       time: times[3].nome
     },
     {
@@ -202,7 +202,7 @@ function App() {
       favorito: false,
       nome: 'Jamal',
       funcao: 'Top laner',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144412398325858344/300531619_1073889343257526_185454562744980554_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153483722402840586/300531619_1073889343257526_185454562744980554_n.png',
       time: times[4].nome
     },
     {
@@ -210,7 +210,7 @@ function App() {
       favorito: false,
       nome: 'Boomer',
       funcao: 'Jungler',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144411960016904223/256876866_580302026515893_5883537445107352451_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153483808012771459/256876866_580302026515893_5883537445107352451_n.png',
       time: times[4].nome
     },
     {
@@ -218,7 +218,7 @@ function App() {
       favorito: false,
       nome: 'Matheusilvbr',
       funcao: 'Mid laner',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144412300044943400/337815895_622660789725586_8959928445761770961_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153483676819132486/337815895_622660789725586_8959928445761770961_n.png',
       time: times[4].nome
     },
     {
@@ -226,7 +226,7 @@ function App() {
       favorito: false,
       nome: 'G10',
       funcao: 'Ad carrys',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144412447126589510/359703128_285786040704095_9207309494618468330_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153483869803261972/370680066_806206877869733_3273304915257091725_n.png',
       time: times[4].nome
     },
     {
@@ -234,7 +234,7 @@ function App() {
       favorito: false,
       nome: 'larisou',
       funcao: 'Suporte',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144412338217295912/340448059_260587522991006_6389602225482235961_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153483989911339099/340448059_260587522991006_6389602225482235961_n.png',
       time: times[4].nome
     },
     
@@ -243,7 +243,7 @@ function App() {
       favorito: false,
       nome: 'Dezi',
       funcao: 'Tank/DPS',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144414978519072768/232207766_516192219653404_5598471225196256884_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484230303686656/232207766_516192219653404_5598471225196256884_n.png',
       time: times[5].nome
     },
     {
@@ -251,7 +251,7 @@ function App() {
       favorito: false,
       nome: 'Bife',
       funcao: 'DPS',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144415204369780866/355216182_821879549447668_883574037821961757_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484276503949433/355216182_821879549447668_883574037821961757_n.png',
       time: times[5].nome
     },
     {
@@ -259,15 +259,15 @@ function App() {
       favorito: false,
       nome: 'Poki',
       funcao: 'DPS',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144419421566283886/jT4_JBHy_400x400.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153534469014294619/6d0d4e19f5ec7810d7e996fa6d1d502d.png',
       time: times[5].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'Vini',
+      nome: 'mdiaz',
       funcao: 'Sup',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144418111433486467/image.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484344040620183/364541184_316648437691203_3465804707211614916_n.png',
       time: times[5].nome
     },
     {
@@ -275,7 +275,7 @@ function App() {
       favorito: false,
       nome: 'Givic',
       funcao: 'Sup',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144417921238569000/341928653_181175084392627_9073001516907346619_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484536798261445/375755062_3445925048991500_3532475584189178786_n.png',
       time: times[5].nome
     },
     {
@@ -283,7 +283,7 @@ function App() {
       favorito: false,
       nome: 'Bocão',
       funcao: 'Lurker/Coringa',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144419935246897173/227779258_4001776999932052_3583539700384187845_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484693191266414/280250692_1211091066361956_4447175069746347915_n.png',
       time: times[6].nome
     },
     {
@@ -291,7 +291,7 @@ function App() {
       favorito: false,
       nome: 'Beirute',
       funcao: 'In-Game Leader/Solo Bomb',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144419814576758834/315791187_493366829600451_6887181696317457223_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484768772636712/315791187_493366829600451_6887181696317457223_n.png',
       time: times[6].nome
     },
     {
@@ -299,7 +299,7 @@ function App() {
       favorito: false,
       nome: 'Fly',
       funcao: 'Awper',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144420293855678494/Odu8S0rm_400x400.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153485150072614983/ab6a7fca3bd2206bafa5e5404539bd77.png',
       time: times[6].nome
     },
     {
@@ -307,14 +307,14 @@ function App() {
       favorito: false,
       nome: 'Gu',
       funcao: 'Entry Fragger/Awper',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144420077651886151/340603426_659399212690309_1421396566537181560_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484817573359646/340603426_659399212690309_1421396566537181560_n.png',
       time: times[6].nome
     },
     {
       id: uuidv4(),
       nome: 'Leozinho',
       funcao: 'Suporte',
-      imagem: 'https://cdn.discordapp.com/attachments/1144000938458222672/1144419870205804634/290000316_167211935780472_3456725659652662117_n.png',
+      imagem: 'https://cdn.discordapp.com/attachments/1144776070214733977/1153484880445964318/290000316_167211935780472_3456725659652662117_n.png',
       time: times[6].nome
     },
   ]
